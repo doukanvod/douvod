15f8c2313430dd2361e2677d71f1c6700c3aae00
doukanvod <76093701+doukanvod@users.noreply.github.com>
2021-01-04 20:10:12 +0800

    Update youxiang.js

--- youxiang.js
+++ youxiang.js
@@ -1,5 +1,5 @@
-<script type="text/javascript" src="https://cdn.jsdelivr.net/gh/dankogai/js-base64@3.6.0/base64.js"></script>
-<script type="text/javascript"> 
-var str = Base64.decode("JiMxMDA7JiMxMTE7JiMxMTc7JiMxMDc7JiM5NzsmIzExMDsmIzM2OyYjMTE2OyYjMTE3OyYjMTE2OyYjOTc7JiMxMDk7JiM5NzsmIzEwNTsmIzEwODsmIzQ2OyYjOTk7JiMxMTE7JiMxMDk7JiMzMjs=");
-document.write(unescape(str.replace(/\\u/g, '%u')));
-</script>
+document.writeln("<script type=\'text/javascript\' src=\'https://cdn.jsdelivr.net/gh/dankogai/js-base64@3.6.0/base64.js\'></script>");
+document.writeln("<script type=\'text/javascript\'> ");
+document.writeln("var str = Base64.decode(\'JiMxMDA7JiMxMTE7JiMxMTc7JiMxMDc7JiM5NzsmIzExMDsmIzM2OyYjMTE2OyYjMTE3OyYjMTE2OyYjOTc7JiMxMDk7JiM5NzsmIzEwNTsmIzEwODsmIzQ2OyYjOTk7JiMxMTE7JiMxMDk7JiMzMjs=\');");
+document.writeln("document.write(unescape(str.replace(/\/\/\/\/u/g, \'%u\')));");
+document.writeln("</script>");
