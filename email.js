dc287a683b164523fb8dd0898d9e18cd9a34d740
doukanvod <76093701+doukanvod@users.noreply.github.com>
2021-01-05 19:55:52 +0800

    Update email.js

--- email.js
+++ email.js
@@ -316,7 +316,7 @@ const gBase64 = {
 }));
 
 //轉碼
-function yx() {
+function email() {
 var dk = Base64.decode('JiMxMDA7JiMxMTE7JiMxMTc7JiMxMDc7JiM5NzsmIzExMDsmIzM2Ow==');
 var hou = Base64.decode('JiMxMTY7JiMxMTc7JiMxMTY7JiM5NzsmIzEwOTsmIzk3OyYjMTA1OyYjMTA4OyYjNDY7JiM5OTsmIzExMTsmIzEwOTs=');
 document.write(unescape(dk.replace(/\\u/g, '%u')) + '<!-- 注釋-->' + unescape(hou.replace(/\\u/g, '%u')));
